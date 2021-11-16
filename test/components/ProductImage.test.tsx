1e7d213b5a3f5926fe916d486f2fd73fb5336fa4
Jesus Alexis <ingjesusalexis@gmail.com>
2021-11-16 12:15:43 -0400

    version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from './data/products';
+
+describe('ProductImage', () => {
+  test('Mostrar componente con la imagen perzonalizada', () => {
+    const wrapper = renderer.create(
+      <ProductImage img="https://hola.jpg" />
+    )
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  test('Debe de mostrar el componente con la imagen del producto', () => {
+    const wrapper = renderer.create(
+    <ProductCard product={product2}>
+      {
+        ()=>(
+          <ProductImage />
+        )
+      }
+    </ProductCard>
+    )
+      expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  
+  
+})
---
+++ test/components/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - Sin Image',
+}
+
+export const product2={
+  id: '2',
+  title: 'Coffee - Con imagen',
+  img: './coffee-mug.png'
+}
